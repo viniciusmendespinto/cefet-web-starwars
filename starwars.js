ae3a7d4d1b9d7f37af8ad7724451dd2a5686d276
viniciusmendespinto <viniciusloco18@hotmail.com>
2017-04-25 12:20:49 -0300

    Sala

--- starwars.js
+++ starwars.js
@@ -3,3 +3,19 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+let episodios = ['I','II','III','IV','V','VI','VII'];
+
+$.ajax({
+  url: 'http://swapi.co/api/films/',
+  method: 'GET',      // opcional: 'GET' é o valor padrão
+  success: function(resposta) {
+  	$('#movies ul').innerHTML="";
+    for(let filmes of result.results){
+        $('#movies ul').append('<li data-episode-url' + filmes.url + '>Episode ' + episodios[filmes.episode_id-1] + '</li');
+    }
+  }
+});
+
+
+
